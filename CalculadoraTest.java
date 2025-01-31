1057b539607930625ef6ee1b4c9f2b40b97d72a3
joel55p <joelnerio42@gmail.com>
2025-01-30 19:08:26 -0600

    final

--- CalculadoraTest.java
+++ CalculadoraTest.java
@@ -6,21 +6,15 @@ class CalculadoraTest {
     @Test
     void testEvaluarPostfixExpresionSimple() {
         Calculadora calculadora = new Calculadora();
-        int resultado = calculadora.evaluar("2 3 +"); /*se guarda el resultado experimetal */
+        int resultado = calculadora.operar(3,2,"+"); /*se guarda el resultado experimetal */
         assertEquals(5, resultado); // como tal verifica que el resultado si sea el correcto osea 5
     }
 
-    @Test
-    void testEvaluarPostfixExpresionCompleja() {  /*se crea un test para una expresion mas compleja que la anterior */
-        Calculadora calculadora = new Calculadora();
-        int resultado = calculadora.evaluar("5 1 2 + 4 * + 3 -");
-        assertEquals(14, resultado);
-    }
 
     @Test
     void testDivisionEntreCero() { /*se crea un test para verificar que no se pueda dividir entre cero, el () -> hace referencia a "ejecutar " es una expresion lambda */
         Calculadora calculadora = new Calculadora(); //osea el () -> calculadora.evaluarPostfix("10 0 /") indica ejecutar metodo  con objeto calculadora y  con el argumento "10 0 /"
-        assertThrows(ArithmeticException.class, () -> calculadora.evaluar("10 0 /")); //se espera que se lance una excepcion de tipo ArithmeticException
+        assertThrows(ArithmeticException.class, () -> calculadora.operar(10,0,"/")); //se espera que se lance una excepcion de tipo ArithmeticException
     }
 
     @Test
--- StackVectorTest.java
+++ StackVectorTest.java
@@ -4,9 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StackVectorTest {
 
     @Test
-    void testPushiPop() {  //se crea un test para verificar que se pueda hacer push y pop de un elemento
+    void testPushiPop() {  /*se crea un test para verificar que se pueda hacer push y pop de un elemento*/
         StackVector<Integer> stack = new StackVector<>();
-        stack.push(10); //se agrega un elemento al stack
+        stack.push(10); /*se agrega un elemento al stack*/
         stack.push(20); //se agrega un elemento al stack
         assertEquals(20, stack.pop());//se espera que el elemento que se elimine sea 20
         assertEquals(10, stack.pop());// se espera que el elemento que se elimine sea 10
@@ -15,14 +15,14 @@ public class StackVectorTest {
     @Test
     void testPeek() {
         StackVector<Integer> stack = new StackVector<>();
-        stack.push(5); //se agrega un elemento al stack
-        stack.push(15); //se agrega un elemento al stack
+        stack.push(5); /*se agrega un elemento al stac  */
+        stack.push(15); /* se agrega un elemento al stack */
         assertEquals(15, stack.peek()); //se espera que ultimo elemento agregado sea 15
     }
 
     @Test
     void testEmpty() {
-        StackVector<Integer> stack = new StackVector<>(); //se crea un stack
+        StackVector<Integer> stack = new StackVector<>(); /*se crea un stack*/
         assertTrue(stack.empty()); //se espera que el stack este vacio osea devuelve un valor true osea boolean
         stack.push(1); //se agrega un elemento al stack
         assertFalse(stack.empty());//se espera que el stack no este vacio osea devuelve un valor false osea boolean
@@ -33,17 +33,17 @@ public class StackVectorTest {
         StackVector<Integer> stack = new StackVector<>();//se crea un stack
         stack.push(5);//se agrega un elemento al stack
         stack.push(10);//se agrega un elemento al stack
-        assertEquals(2, stack.size());//se espera que el tamaño del stack sea 2, ya que se agregaron 2 elementos
+        assertEquals(2, stack.size());/*se espera que el tamaño del stack sea 2, ya que se agregaron 2 elementos*/
     }
 
     @Test
     void testPopOnEmptyStack() { 
         StackVector<Integer> stack = new StackVector<>();// se va  usar el metodo pop en un stack vacio por lo que se espera que se lance una excepcion
-        assertThrows(IllegalStateException.class, stack::pop); // en este caso el :: hace referencia al metodo pop de la intancia stack, es en vez de usar () -> stack.pop()
+        assertThrows(IllegalStateException.class, stack::pop); /*  en este caso el :: hace referencia al metodo pop de la intancia stack, es en vez de usar () -> stack.pop()*/
     }
 
     @Test
-    void testPeekOnEmptyStack() { //se va  usar el metodo peek en un stack vacio por lo que se espera que se lance una excepcion
+    void testPeekOnEmptyStack() { /*se va  usar el metodo peek en un stack vacio por lo que se espera que se lance una excepcion*/
         StackVector<Integer> stack = new StackVector<>();
         assertThrows(IllegalStateException.class, stack::peek);//lo miso que en el test anterior se usa :: en vez de () ->
     }
